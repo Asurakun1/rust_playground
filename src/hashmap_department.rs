c920199ac6b039a62fd925e1352b9488fc0f67fa
Asurakun1 <udon3699@gmail.com>
2023-02-03 22:54:05 -0800

    more hashmapping

---
+++ src/hashmap_department.rs
@@ -0,0 +1,36 @@
+pub mod hashmap_department {
+    use rand::{thread_rng, Rng};
+    use std::{collections::HashMap, io};
+    pub fn department(employees: Vec<&str>, departments: Vec<&str>, depar: &str) -> Vec<(String, String)> {
+
+        let mut map: HashMap<String, String> = HashMap::new();
+        for employee in &employees {
+            map.insert(
+                employee.to_string(),
+                departments[thread_rng().gen_range(0..departments.len())].to_string(),
+            );
+        }
+    
+        let tech_filter: HashMap<String, String> = map.into_iter().filter(|(_employee, department)| *department == depar).collect();
+        let mut tech_sort: Vec<(String, String)> = tech_filter.into_iter().collect();
+        tech_sort.sort();
+        tech_sort
+    }
+    
+    fn _add_employee() -> String {
+        println!("Add employee: ");
+        let mut input = String::from("");
+        io::stdin().read_line(&mut input).unwrap();
+        input = input.trim().to_string();
+        input
+    }
+    
+    fn _add_department() -> String {
+        println!("to department: ");
+        let mut input = String::from("");
+        io::stdin().read_line(&mut input).unwrap();
+        input = input.trim().to_string();
+        input
+    }
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use rand::{thread_rng, Rng};
-use std::{collections::HashMap, io, vec};
+mod hashmap_department;
+use crate::hashmap_department::hashmap_department::department;
 fn main() {
-    let mut _employees: Vec<&str> = vec![
+    let employees: Vec<&str> = vec![
         "Hannah",
         "Ethan",
         "Emma",
@@ -14,31 +14,6 @@ fn main() {
         "James",
     ];
 
-    let _departments = vec!["Tech", "Sales", "Accounting", "Cooking", "Cleaning"];
-    let mut employee_map: HashMap<String, String> = HashMap::new();
-
-    for employee in &_employees {
-        employee_map.insert(
-            employee.to_string(),
-            _departments[thread_rng().gen_range(0.._departments.len())].to_string(),
-        );
-    }
-    let tech_filter: &HashMap<String, String> = &employee_map.into_iter().filter(|(_employee, department)| *department == "Tech").collect();
-    println!("{:#?}", tech_filter);
-}
-
-fn _add_employee() -> String {
-    println!("Add employee: ");
-    let mut input = String::from("");
-    io::stdin().read_line(&mut input).unwrap();
-    input = input.trim().to_string();
-    input
-}
-
-fn _add_department() -> String {
-    println!("to department: ");
-    let mut input = String::from("");
-    io::stdin().read_line(&mut input).unwrap();
-    input = input.trim().to_string();
-    input
+    let departments = vec!["Tech", "Sales", "Accounting", "Cooking", "Cleaning"];
+    println!("{:?}", department(employees, departments, "Cooking"));
 }
