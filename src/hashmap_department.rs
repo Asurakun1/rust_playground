dc1bce2e1f7ec36308078725ae44a334498410ba
Asurakun1 <udon3699@gmail.com>
2023-02-05 02:02:07 -0800

    progress in rust language

--- src/hashmap_department.rs
+++ src/hashmap_department.rs
@@ -1,8 +1,11 @@
 pub mod hashmap_department {
     use rand::{thread_rng, Rng};
     use std::{collections::HashMap, io};
-    pub fn department(employees: Vec<&str>, departments: Vec<&str>, depar: &str) -> Vec<(String, String)> {
-
+    pub fn department(
+        employees: Vec<&str>,
+        departments: Vec<&str>,
+        depar: &str,
+    ) -> Vec<(String, String)> {
         let mut map: HashMap<String, String> = HashMap::new();
         for employee in &employees {
             map.insert(
@@ -10,13 +13,16 @@ pub mod hashmap_department {
                 departments[thread_rng().gen_range(0..departments.len())].to_string(),
             );
         }
-    
-        let tech_filter: HashMap<String, String> = map.into_iter().filter(|(_employee, department)| department == depar).collect();
+
+        let tech_filter: HashMap<String, String> = map
+            .into_iter()
+            .filter(|(_employee, department)| department == depar)
+            .collect();
         let mut tech_sort: Vec<(String, String)> = tech_filter.into_iter().collect();
         tech_sort.sort();
         tech_sort
     }
-    
+
     fn _add_employee() -> String {
         println!("Add employee: ");
         let mut input = String::from("");
@@ -24,7 +30,7 @@ pub mod hashmap_department {
         input = input.trim().to_string();
         input
     }
-    
+
     fn _add_department() -> String {
         println!("to department: ");
         let mut input = String::from("");
@@ -32,5 +38,4 @@ pub mod hashmap_department {
         input = input.trim().to_string();
         input
     }
-    
 }
---
+++ src/tests.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 1;
+        assert_eq!(result, 4);
+    }
+
+
+    #[test]
+    fn it_doesnt_work() {
+        let result = 2 + 1;
+        assert_eq!(result, 4);
+    }
+}
